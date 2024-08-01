d9c0702a4a7eebe7c24891a24733db66e2298aff
hihimamuLab <hihimamug14@gmail.com>
2024-08-01 12:46:51 +0900

    UPDATE: implemented loss function api::loss_function::cross_entropy_error()

--- src/api.rs
+++ src/api.rs
@@ -13,25 +13,36 @@ pub mod activate_functions {
     }
 
     pub fn softmax(neuron_layer: NeuronLayer) -> NeuronLayer {
-        let maximum_value: f64 = neuron_layer
-            .iter()
-            .map(|v| *v)
-            .reduce(f64::max)
-            .unwrap();
+        let maximum_value: f64 = neuron_layer.iter().map(|v| *v).reduce(f64::max).unwrap();
         let sum_e_pow_element: f64 = neuron_layer
             .iter()
             .map(|v| Napier.powf(v - maximum_value))
             .sum();
         neuron_layer
             .iter()
-            .map(|v| {
-                Napier.powf(v - maximum_value) / sum_e_pow_element
-            })
+            .map(|v| Napier.powf(v - maximum_value) / sum_e_pow_element)
             .collect()
     }
 }
 
-use crate::{DatasetVector, NeuronLayer};
+pub mod loss_function {
+    use crate::{DatasetVector, NeuronLayer, LABEL_LEN};
+    use std::f64::consts::E as Napier;
+
+    pub fn cross_entropy_error(neuron_layer: Vec<NeuronLayer>, lbl: DatasetVector) -> f64 {
+        let sum_loss: f64 = ((0..neuron_layer.len())
+            .map(|i| {
+                (0..LABEL_LEN)
+                    .map(|j| neuron_layer[i][j] * (lbl[i][j] as f64).log(Napier))
+                    .sum::<f64>()
+            })
+            .sum::<f64>());
+
+        -(sum_loss) / neuron_layer.len() as f64
+    }
+}
+
+use crate::DatasetVector;
 pub fn reshape(mut vector: Vec<f32>, height: usize, width: usize) -> DatasetVector {
     let mut _cut_vector: Vec<f32> = vec![];
     let mut reshaped_vector: DatasetVector = vec![];
